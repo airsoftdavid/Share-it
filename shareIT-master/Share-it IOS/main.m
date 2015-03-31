007a928f47e274f2b6c176dc8f5650b490553e85
David Chen <airsoftdavid@gmail.com>
2015-03-31 02:25:50 -0400

    test commit from Virtual Machine
    test commit from Virtual Machine

---
+++ shareIT-master/Share-it IOS/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Share-it
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#import "M2AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([M2AppDelegate class]));
+  }
+}
